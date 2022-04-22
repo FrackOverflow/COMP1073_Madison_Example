eac48d36bda2e558e39a313fe693e9ddf85bab49
FrackOverflow <madison.hd.pardy@gmail.com>
2022-04-22 18:43:56 -0300

    More wk1 setup

--- Wk1 Code Samples/Basic_Grammar.js
+++ Wk1 Code Samples/Basic_Grammar.js
@@ -15,19 +15,20 @@ var b = 2 // No semicolon required
 
 var a = 1; var b = 2 // Semicolon required to seperate statements
 
+// Variables
 var uninitialized;
 
 let myVar = 1;
 
 // whats the difference between var and let? You can't redeclare a 'let'
 
-var c = 'fizz'
-var c = 'buzz' // No issue here
-let d = 'bizz'
-let d = 'fuzz' // Syntax Error
+var c = 'fizz';
+var c = 'buzz'; // No issue here
+let d = 'bizz';
+let d = 'fuzz'; // Syntax Error
 
 // Constants can't be uninitialized or changed
-const noon = "12 pm"
+const noon = "12 pm";
 
 // Sources:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types
