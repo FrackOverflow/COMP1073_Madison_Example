d409e894a64f973c3f93f73b0277e103b2d05ab0
FrackOverflow <madison.hd.pardy@gmail.com>
2022-04-23 21:12:34 -0300

    Wk3 examples array methods

--- Code_Samples/Wk3_Code_Samples/Data_Structures.js
+++ Code_Samples/Wk3_Code_Samples/Data_Structures.js
@@ -58,27 +58,47 @@ console.log(fruit.join());      // Apple,Orange,Banana,Tomato
 console.log(fruit.join(""));    // AppleOrangeBananaTomato
 console.log(fruit.join(" - ")); // Apple - Orange - Banana - Tomato
 
-// Push: Adds one or more elemnts to the end of the array and returns the resulting array length
-let myArray = new Array('1', '2')
+// Push: adds one or more elemnts to the end of the array and returns the resulting array length
+var myArray = new Array('1', '2')
 myArray.push('3')  // myArray is now ["1", "2", "3"]
 
-
 // Pop: removes the last element from an array and returns it
-let myArray = new Array('1', '2', '3')
-let last = myArray.pop()
+var myArray = new Array('1', '2', '3')
+var last = myArray.pop()
 // myArray is now ["1", "2"], last = "3"
 
+// Shift: removes the first element from an array and returns it
+let myArray = new Array('1', '2', '3')
+let first = myArray.shift()
+// myArray is now ["2", "3"], first is "1"
 
-// Shift
+// Unshift: adds one or more elements to the front of an array and returns the new length
+let myArray = new Array('1', '2', '3')
+myArray.unshift('4', '5')
+// myArray becomes ["4", "5", "1", "2", "3"]
 
-// Unshift
+// Slice: extracts a section of an array and returns the new array
+let myArray = new Array('a', 'b', 'c', 'd', 'e')
+myArray = myArray.slice(1, 4)  // starts at index 1 and extracts all elements
+                               // until index 3, returning [ "b", "c", "d"]
 
-// Slice: Extracts a section of an array and returns the new array
+// Splice: removes elements from an array and optionally replaces them, returns the removed elements
+let myArray = new Array('1', '2', '3', '4', '5')
+myArray.splice(1, 3, 'a', 'b', 'c', 'd')
+// myArray is now ["1", "a", "b", "c", "d", "5"]
+// This code started at index one (or where the "2" was),
+// removed 3 elements there, and then inserted all consecutive
+// elements in its place.
 
-// Splice:
+// Reverse: reverses the order of the array
+let myArray = new Array('1', '2', '3')
+myArray.reverse()
+// transposes the array so that myArray = ["3", "2", "1"]
 
-
-
+// Sort: sorts the array elements in place and returns the result
+let myArray = new Array('Wind', 'Rain', 'Fire')
+myArray.sort()
+// sorts the array so that myArray = ["Fire", "Rain", "Wind"]
 
 /*** Keyed Collections ***/
 // Map
