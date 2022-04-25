0b4d4ade13a30bfa7e9136a9ae487f372aeecbe9
FrackOverflow <madison.hd.pardy@gmail.com>
2022-04-25 19:27:32 -0300

    Keyed Collection Examples

--- Code_Samples/Wk3_Code_Samples/Data_Structures.js
+++ Code_Samples/Wk3_Code_Samples/Data_Structures.js
@@ -101,10 +101,38 @@ myArray.sort()
 // sorts the array so that myArray = ["Fire", "Rain", "Wind"]
 
 /*** Keyed Collections ***/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Keyed_collections
 // Map
+let sayings = new Map();
+sayings.set('dog', 'woof');
+sayings.set('cat', 'meow');
+sayings.set('elephant', 'toot');
+sayings.size; // 3
+sayings.get('dog'); // woof
+sayings.get('fox'); // undefined
+sayings.has('bird'); // false
+sayings.delete('dog');
+sayings.has('dog'); // false
+
+for (let [key, value] of sayings) {
+  console.log(key + ' goes ' + value);
+}
+// "cat goes meow"
+// "elephant goes toot"
+
+sayings.clear();
+sayings.size; // 0
 
 // Set
+let mySet = new Set();
+mySet.add(1);
+mySet.add('some text');
+mySet.add('foo');
 
-// Object
+mySet.has(1); // true
+mySet.delete('foo');
+mySet.size; // 2
 
-// Dict
+for (let item of mySet) console.log(item);
+// 1
+// "some text"
